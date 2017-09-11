3896ce0a92a46aaa056bf11d06661be4df3e7f10
Cameron Ellis <Cameron@CameronEllis-PC.(none)>
2017-09-11 15:41:36 -0700

    server side js. Removed mongodb code that's not needed right now as I fix Express

--- task_assigner/app.js
+++ task_assigner/app.js
@@ -1,8 +1,30 @@
 var express = require('express');
 var app = express();
-var mongo = require('mongodb');
+
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/task_assigner";
+
+var bodyParser = require('body-parser');
 
 app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
+
+app.post('/resultList', function(req, res){
+	console.log("Is this working?");
+
+	console.log(req);
+
+	// put mongo db manipulation code here
+	// MongoClient.connect(url, function(err, db) {
+	//   if (err) throw err;
+	//   var myobj = { name: "Cameron", address: "1600 Pennsylvania Avenue" };
+	//   db.collection("resultList").insertOne(myobj, function(err, res) {
+	//     if (err) throw err;
+	//     console.log("1 document inserted");
+	//     db.close();
+	//   });
+	// });
+});
 
 app.listen(3000);
 console.log("Server running on port 3000");
